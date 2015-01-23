2ae6fcc8e5a3e60b5060906cb6bdb326ea440027
Patrick Stephen <PJ@PJs-MacBook-Air.local>
2015-01-23 17:57:42 -0600

    Switched over from cloud pebble to Pebble SDK

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 7 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Simple",
+  .company = "pjtnt11",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x13, 0x37, 0x13, 0x37, 0xD5, 0x79, 0x4D, 0x75, 0xA5, 0xC5, 0x8D, 0xFC, 0xFE, 0x11, 0x0F, 0x62 },
+  .virtual_size = 0xb6b6
+};
